d6ca7d6c125f4c8a42deb2f48848edb9016fdba0
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-07-09 23:21:02 +0530

    added more sections

--- js/index.js
+++ js/index.js
@@ -12,3 +12,83 @@ const menu = document.querySelector(".menu");
 navbarToggle.addEventListener("click", () => {
   menu.classList.toggle("show");
 });
+
+async function fetchData() {
+  try {
+    const response = await fetch('data.json');
+    const data = await response.json();
+    return data.features;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return [];
+  }
+}
+
+function createFeatureCards(features) {
+  const featureContainer = document.getElementById('feature-container');
+
+  features.forEach((feature) => {
+    const featureCard = document.createElement('div');
+    featureCard.className = 'FeatureCard';
+
+    const featureContent = document.createElement('div');
+
+    const featureTitle = document.createElement('h3');
+    featureTitle.className = 'FeatureTitle';
+    featureTitle.textContent = feature.name;
+
+    const featureDescription = document.createElement('p');
+    featureDescription.className = 'FeatureCardDescription';
+    featureDescription.textContent = feature.desc;
+
+    featureContent.appendChild(featureTitle);
+    featureContent.appendChild(featureDescription);
+
+    const featureIcon = document.createElement('div');
+    featureIcon.className = 'FeatureIcon';
+
+    featureCard.appendChild(featureContent);
+    featureCard.appendChild(featureIcon);
+
+    featureContainer.appendChild(featureCard);
+  });
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const features = await fetchData();
+  createFeatureCards(features);
+});
+
+async function fetchContactData() {
+  try {
+    const response = await fetch('data.json');
+    const data = await response.json();
+    return data.contact;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return {};
+  }
+}
+
+function updateContactInfo(contact) {
+  const contactInfoContainer = document.getElementById('contact-info');
+
+  const email = document.createElement('p');
+  email.innerHTML = `<strong style="color: hsl(280, 80%, 59%);">Email:</strong> ${contact.email}`;
+
+  const phone = document.createElement('p');
+  phone.innerHTML = `<strong style="color: hsl(280, 80%, 59%);">Phone:</strong> ${contact.phone}`;
+
+  const address = document.createElement('p');
+  address.innerHTML = `<strong style="color: hsl(280, 80%, 59%);">Address:</strong> ${contact.address}`;
+
+  contactInfoContainer.appendChild(email);
+  contactInfoContainer.appendChild(phone);
+  contactInfoContainer.appendChild(address);
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const contact = await fetchContactData();
+  updateContactInfo(contact);
+});
+
